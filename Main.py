ffe250bce57fed73265a76b47960d72b67ae3111
Mikhail Zhebrunov <79290631+1399999@users.noreply.github.com>
2026-04-24 12:01:47 -0400

    Modify UI layout and button labels in Main.py
    Updated UI elements and labels for better clarity.

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import time
 
 root = tk.Tk()
 root.title("Input Taker")
-root.geometry("300x200")
+root.geometry("300x300")
 
 start = 0.0
 length = 0.0
@@ -15,21 +15,24 @@ def Take_input():
 def Take_output():
     end = time.time()
     length = end - start
-    greeting2.config(text=str(length))
+    greeting2.config(text="Time: " + str(length))
+    greeting3.config(text="Output: " + T.get("1.0",tk.END))
 
-greeting = tk.Label(text="Enter your student ID:")
+greeting = tk.Label(text="Enter student ID:")
 greeting.pack(pady=20)
 
 T = tk.Text(root, height = 1, width = 10)
 
-b1 = tk.Button(root, text = "Time Start", command = Take_input)
-b2 = tk.Button(root, text = "Time End", command = Take_output)
+b1 = tk.Button(root, text = "Submit, Start Time", command = Take_input)
+b2 = tk.Button(root, text = "End Time", command = Take_output)
 
-greeting2 = tk.Label(text=length)
+greeting2 = tk.Label(text="Time: " + str(length))
+greeting3 = tk.Label(text="Output: " + str(length))
 
 T.pack()
 b1.pack(pady=20)
 b2.pack(pady=10)
 greeting2.pack(pady=20)
+greeting3.pack(pady=20)
 
 root.mainloop()
