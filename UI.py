e6e7f24de8b330179bc179301eafca7755885b41
SmhxFaun <cfaunlol@gmail.com>
2026-04-28 11:58:28 -0400

    fix UI.py

--- UI.py
+++ UI.py
@@ -1,34 +1,38 @@
 import tkinter as tk
 import time
 
-class UI():
-    global root, start, length, T
+class UI:
+
+    # root = None
+    # time_str = None
+    # greeting2 = None
 
     def __init__(self):
-        root = tk.Tk()
-        root.title("Input Taker")
-        root.geometry("300x300")
+
+        self.root = tk.Tk()
+        self.root.title("Input Taker")
+        self.root.geometry("300x300")
         self.start = 0.0
         self.length = 0.0
 
-        greeting = tk.Label(text="Enter student ID:")
-        greeting.pack(pady=20)
+        self.greeting = tk.Label(text="Enter student ID:")
+        self.greeting.pack(pady=20)
 
-        T = tk.Text(root, height = 1, width = 10)
+        self.T = tk.Text(self.root, height = 1, width = 10)
 
-        b1 = tk.Button(root, text = "Submit, Start Time", command = self.Take_input)
-        b2 = tk.Button(root, text = "End Time", command = self.Take_output)
+        self.b1 = tk.Button(self.root, text = "Submit, Start Time", command = self.Take_input)
+        self.b2 = tk.Button(self.root, text = "End Time", command = self.Take_output)
 
-        greeting2 = tk.Label(text="Time: " + str(length))
-        greeting3 = tk.Label(text="Output: " + str(length))
+        self.greeting2 = tk.Label(text="Time: " + str(self.length))
+        self.greeting3 = tk.Label(text="Output: " + str(self.length))
 
-        T.pack()
-        b1.pack(pady=20)
-        b2.pack(pady=10)
-        greeting2.pack(pady=20)
-        greeting3.pack(pady=20)
+        self.T.pack()
+        self.b1.pack(pady=20)
+        self.b2.pack(pady=10)
+        self.greeting2.pack(pady=20)
+        self.greeting3.pack(pady=20)
 
-        root.mainloop()
+        self.root.mainloop()
 
     def Take_input(self):
         self.start = time.time()
